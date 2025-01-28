c9d06afd76054365ea8e742df35bdf81fa62b7e3
LilChihba <danila-yurov@mail.ru>
2025-01-28 14:15:02 +0300

    added repositories

---
+++ app/src/main/java/com/example/headhunter/repository/OfferRepository.kt
@@ -0,0 +1,9 @@
+package com.example.headhunter.repository
+
+import com.example.headhunter.model.Offer
+
+class OfferRepository {
+    suspend fun getOffer(): List<Offer> {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/headhunter/repository/VacancyRepository.kt
@@ -0,0 +1,9 @@
+package com.example.headhunter.repository
+
+import com.example.headhunter.model.Vacancy
+
+class VacancyRepository {
+    suspend fun getVacancy(): List<Vacancy> {
+        TODO()
+    }
+}
