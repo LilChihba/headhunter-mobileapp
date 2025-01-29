3da2803b86316ea3b11449cd38ad12ff17614c5e
LilChihba <danila-yurov@mail.ru>
2025-01-29 13:59:11 +0300

    changed dark theme

--- app/src/main/java/com/example/headhunter/ui/theme/Theme.kt
+++ app/src/main/java/com/example/headhunter/ui/theme/Theme.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.headhunter.ui.theme
 
 import android.app.Activity
@@ -9,18 +11,22 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    secondary = White,
+    tertiary = Shadows
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Purple80,
+    secondary = White,
+    tertiary = Shadows
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,9 +51,19 @@ fun HeadhunterTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.tertiary.toArgb()
+            window.navigationBarColor = colorScheme.tertiary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = false
+        }
     }
 
     MaterialTheme(
